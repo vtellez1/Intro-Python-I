dd411093167ad464dbc9f1520a30fc2adcb6b273
Vanessa <vanessatellez45@gmail.com>
2020-04-06 18:07:38 -0400

    half done

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,6 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+print(2 ** 65536)
+
+# When doing console.log(2 ** 65536) or console.log(Math.pow(2, 65536)) in JS console, received "Infinity" followed by undefined.
